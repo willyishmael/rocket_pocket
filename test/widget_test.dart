6d728caf1c80beabd56deac0e7fde0355fa2811b
William Yishmael <wtangka22@gmail.com>
2025-04-25 11:37:58 +0900

    Setup main.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:rocket_pocket/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const RocketPocket());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
