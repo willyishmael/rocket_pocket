ca23fa1bc84ebc93f046b85900543523a4998ac5
William Yishmael <wtangka22@gmail.com>
2025-05-08 14:02:31 +0900

    Create screens.dart

---
+++ lib/screens/screens.dart
@@ -0,0 +1,9 @@
+// This file exports all the screens in the app.
+// This allows for easy access to all screens from a single file.
+
+export 'budget/budget_screen.dart';
+export 'dashboard/dashboard_screen.dart';
+export 'root/root_screen.dart';
+export 'settings/settings_screen.dart';
+export 'transaction/add_transaction_screen.dart';
+export 'transaction/transaction_screen.dart';
