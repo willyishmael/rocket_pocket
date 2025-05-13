10b9953b9bbc706b3b0c294ad4597a0f70d3d8f9
William Yishmael <wtangka22@gmail.com>
2025-05-13 11:57:13 +0900

    Update create_account_screen.dart

--- lib/screens/dashboard/create_account_screen.dart
+++ lib/screens/dashboard/create_account_screen.dart
@@ -1,8 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:rocket_pocket/data/model/account.dart';
 
-class CreateAccountScreen extends StatelessWidget {
+class CreateAccountScreen extends StatefulWidget {
   const CreateAccountScreen({super.key});
+
+  @override
+  State<CreateAccountScreen> createState() => _CreateAccountScreenState();
+}
+
+class _CreateAccountScreenState extends State<CreateAccountScreen> {
+  final Account account = Account(
+    id: 1,
+    name: 'William',
+    balance: 1000.0,
+    currency: 'USD',
+    accentColor: Colors.blue.value,
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -23,6 +37,7 @@ class CreateAccountScreen extends StatelessWidget {
               title: Text('Create Account'),
             ),
           ),
+          SliverToBoxAdapter(child: AccountCard(account: account)),
           SliverToBoxAdapter(
             child: Padding(
               padding: const EdgeInsets.all(16.0),
@@ -35,13 +50,12 @@ class CreateAccountScreen extends StatelessWidget {
                       border: OutlineInputBorder(),
                       icon: const Icon(Icons.add_card),
                     ),
-
                   ),
                   TextField(
                     decoration: InputDecoration(
                       labelText: 'Account Balance',
                       border: OutlineInputBorder(),
-                      icon: const Icon(Icons.money)
+                      icon: const Icon(Icons.money),
                     ),
                     keyboardType: TextInputType.number,
                   ),
@@ -83,6 +97,77 @@ class CreateAccountScreen extends StatelessWidget {
           ),
           // Add your form or other widgets here
         ],
+      ),
+    );
+  }
+}
+
+class AccountCard extends StatelessWidget {
+  final Account account;
+  const AccountCard({super.key, required this.account});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(16.0),
+      decoration: BoxDecoration(
+        gradient: const LinearGradient(
+          colors: [Colors.blue, Colors.purple],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+        borderRadius: BorderRadius.circular(16.0),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.2),
+            blurRadius: 8.0,
+            offset: const Offset(0, 4),
+          ),
+        ],
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    account.name,
+                    style: const TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  const SizedBox(height: 32.0),
+                  Text(
+                    account.balance.toString(),
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  const SizedBox(height: 16.0),
+                  Text(
+                    account.currency,
+                    style: const TextStyle(fontSize: 18.0, color: Colors.white),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(width: 16.0),
+            Column(
+              children: [
+                Icon(Icons.arrow_forward, color: Colors.white, size: 28.0),
+                Icon(Icons.credit_card, color: Colors.white, size: 28.0),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
