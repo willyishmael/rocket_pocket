16c17fa22d809fc7de0807b8f0e97dbbc8844062
William Yishmael <wtangka22@gmail.com>
2025-05-29 22:21:55 +0900

    Update color_gradient.dart

--- lib/data/model/color_gradient.dart
+++ lib/data/model/color_gradient.dart
@@ -1,20 +1,54 @@
+import 'dart:convert';
 import 'dart:ui';
+import 'package:drift/drift.dart';
+import '../local/database.dart' as db_provider;
 
+// Custom gradient representation for UI
 class ColorGradient {
+  final int id;
   final String name;
   final List<Color> colors;
+  final DateTime createdAt;
 
-  ColorGradient({required this.name, required this.colors});
+  ColorGradient({
+    required this.id,
+    required this.name,
+    required this.colors,
+    required this.createdAt,
+  });
 
-  ColorGradient.fromJson(Map<String, dynamic> json)
-    : name = json['name'] as String,
-      colors =
-          (json['colors'] as List<dynamic>)
-              .map((color) => Color(color as int))
-              .toList();
+  // Factory constructor to create a ColorGradient from a database row
+  static ColorGradient fromDb(db_provider.ColorGradient dbGradient) {
+    return ColorGradient(
+      id: dbGradient.id,
+      name: dbGradient.name,
+      colors: dbGradient.colors,
+      createdAt: dbGradient.createdAt,
+    );
+  }
 
-  Map<String, dynamic> toJson() => {
-    'name': name,
-    'colors': colors.map((color) => color.toARGB32()).toList(),
-  };
+  // Converts the ColorGradient to a database-friendly format
+  db_provider.ColorGradient toDb() {
+    return db_provider.ColorGradient(
+      id: id,
+      name: name,
+      colors: colors,
+      createdAt: createdAt,
+    );
+  }
+}
+
+class ColorListConverter extends TypeConverter<List<Color>, String> {
+  const ColorListConverter();
+
+  @override
+  List<Color> fromSql(String fromDb) {
+    final List<dynamic> jsonList = jsonDecode(fromDb);
+    return jsonList.map((value) => Color(value as int)).toList();
+  }
+
+  @override
+  String toSql(List<Color> value) {
+    return jsonEncode(value.map((color) => color.toARGB32()).toList());
+  }
 }
